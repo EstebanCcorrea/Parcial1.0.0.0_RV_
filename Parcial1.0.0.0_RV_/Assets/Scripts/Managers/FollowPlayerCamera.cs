380af1bcf82e02607b5aa90765e5ea3b980f34a5
Salax-18 <katheduque08@gmail.com>
2026-09-08 22:38:13 -0500

    Camera
    añadi zoom

--- Parcial1.0.0.0_RV_/Assets/Scripts/Managers/FollowPlayerCamera.cs
+++ Parcial1.0.0.0_RV_/Assets/Scripts/Managers/FollowPlayerCamera.cs
@@ -7,12 +7,19 @@ public class FollowPlayerCamera : MonoBehaviour
     [SerializeField] private Transform target;
 
     [Header("Distancia")]
-    [SerializeField] private float distance = 8f;
-    [SerializeField] private float height = 3f;
+    [SerializeField] private float distance = 5f;
+    [SerializeField] private float minDistance = 3f;
+    [SerializeField] private float maxDistance = 8f;
+
+    [Header("Altura")]
+    [SerializeField] private float height = 2.5f;
 
     [Header("Rotación")]
-    [SerializeField] private float sensitivity = 120f;
+    [SerializeField] private float sensitivity = 100f;
     [SerializeField] private float smoothSpeed = 8f;
+
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed = 2f;
 
     private Vector2 lookInput;
 
@@ -21,17 +28,53 @@ public class FollowPlayerCamera : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext context)
     {
+       
+        // El mouse se maneja directamente para ambas cámaras.
+        if (context.control.device is Mouse)
+            return;
+
         lookInput = context.ReadValue<Vector2>();
     }
 
     private void LateUpdate()
     {
+      
+        // este es para el MOUSE
+       
+
+        if (Mouse.current != null)
+        {
+            Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+            lookInput = mouseDelta;
+        }
+
+
+        // El manejo de la rotación de la cámara se hace en LateUpdate para que se ejecute después de que el jugador se haya movido.
+
+
         yaw += lookInput.x * sensitivity * Time.deltaTime;
         pitch -= lookInput.y * sensitivity * Time.deltaTime;
 
         pitch = Mathf.Clamp(pitch, -20f, 60f);
 
         Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+
+       
+        // ZOOM
+      
+
+        if (Mouse.current != null)
+        {
+            float scroll = Mouse.current.scroll.ReadValue().y;
+
+            distance -= scroll * zoomSpeed * Time.deltaTime;
+            distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        }
+
+    
+        // POSICIÓN
+        
 
         Vector3 offset = rotation * new Vector3(0, height, -distance);
 
